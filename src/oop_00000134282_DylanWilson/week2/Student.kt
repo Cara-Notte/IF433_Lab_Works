b3cf61795854ebcf9b51aa11fc4aa7d219487c6f
Caranottella <caranottella@gmail.com>
2026-02-09 13:55:21 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000134282_DylanWilson/week2/Student.kt
+++ src/oop_00000134282_DylanWilson/week2/Student.kt
@@ -1,6 +1,11 @@
 package oop_00000134282_DylanWilson.week2
 
-class Student (val name: String, val nim: String, var major: String) {
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String,
+    var gpa: Double = 0.0
+) {
 
     constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
         println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
@@ -11,7 +16,7 @@ class Student (val name: String, val nim: String, var major: String) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
-            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+            println("LOG: Objek Student $name dengan jurusan $major dan gpa $gpa berhasil dialokasikan di Memory.")
         }
     }
 }
