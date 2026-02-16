95d63855fe6d60a006096c6cd8c06fb8968f9e1a
Caranottella <caranottella@gmail.com>
2026-02-16 18:05:55 +0700

    week03: (task 2) implement Player testing in main

---
+++ src/oop_00000134282_DylanWilson/week3/MainPlayer.kt
@@ -0,0 +1,11 @@
+package oop_00000134282_DylanWilson.week3
+
+fun main() {
+    val player = Player("Cara")
+
+    player.addXp(50)
+
+    player.addXp(60)
+
+    println("Level saat ini: ${player.level}")
+}
