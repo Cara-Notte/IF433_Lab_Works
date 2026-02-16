c27b4d0a72bc9e7de2124f39f8ad78c0ab3253ee
Caranottella <caranottella@gmail.com>
2026-02-16 17:21:18 +0700

    week03: cleanup code

--- src/oop_00000134282_DylanWilson/week3/Employee.kt
+++ src/oop_00000134282_DylanWilson/week3/Employee.kt
@@ -2,6 +2,7 @@ package oop_00000134282_DylanWilson.week3
 
 class Employee(val name: String) {
     var salary: Int = 0
+    private var performanceRating: Int = 3
 
     set(value) {
         if (value < 0) {
@@ -11,8 +12,6 @@ class Employee(val name: String) {
             field = value
         }
     }
-
-    private var performanceRating: Int = 3
 
     fun increasePerformance(){
         performanceRating++
