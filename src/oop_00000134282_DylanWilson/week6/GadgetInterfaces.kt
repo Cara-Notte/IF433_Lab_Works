919181d0415faf49636f6b62080cd66ab4ee3eb2
Caranottella <caranottella@gmail.com>
2026-03-09 14:18:06 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000134282_DylanWilson/week6/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000134282_DylanWilson.week6
+
+interface Camera {
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
