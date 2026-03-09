f9fe128b1c401b089bc6673a93c03ca763c68ed6
Caranottella <caranottella@gmail.com>
2026-03-09 15:36:26 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_00000134282_DylanWilson/week6/SmartSpeaker.kt
@@ -0,0 +1,19 @@
+package oop_00000134282_DylanWilson.week6
+
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("$name dinyalakan.")
+    }
+
+    override fun turnOff() {
+        println("$name dimatikan.")
+    }
+
+    fun playMusic(song: String) {
+        println("Memutar lagu $song dari Spotify.")
+    }
+}
