71f625ef1d96c67a89b0458f9f6d218b400c8001
Caranottella <caranottella@gmail.com>
2026-04-13 16:00:17 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000134282_DylanWilson/week8/EcommerceModels.kt
@@ -0,0 +1,15 @@
+package oop_00000134282_DylanWilson.week8
+
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
