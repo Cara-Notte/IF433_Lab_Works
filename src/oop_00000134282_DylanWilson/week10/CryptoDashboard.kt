39ce2cc754bab01af01c1b1078c887086fbb73c9
Caranottella <caranottella@gmail.com>
2026-04-27 12:50:37 +0700

    week10: (task) populate coin repository

--- src/oop_00000134282_DylanWilson/week10/CryptoDashboard.kt
+++ src/oop_00000134282_DylanWilson/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000134282_DylanWilson.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.75))
+    coinRepo.add(Coin("ETH", 3.5))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
