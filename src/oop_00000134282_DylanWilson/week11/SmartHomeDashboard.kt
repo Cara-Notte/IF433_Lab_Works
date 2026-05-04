2a3dfec4b31c204d91cd6ac97d7b9b5f4815c04d
Caranottella <caranottella@gmail.com>
2026-05-04 14:52:32 +0700

    week11: (task) implement device search with let

--- src/oop_00000134282_DylanWilson/week11/SmartHomeDashboard.kt
+++ src/oop_00000134282_DylanWilson/week11/SmartHomeDashboard.kt
@@ -26,4 +26,13 @@ fun main() {
 
     homeDevices.add(acInverter)
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find {
+        it.category == "Camera"
+    }
+
+    searchResult?.let {
+        println("\n=== HASIL PENCARIAN CAMERA ===")
+        println(it.diagnose())
+    }
 }
